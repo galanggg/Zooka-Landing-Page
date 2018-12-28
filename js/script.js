afec415ea4fca30bdf650f348efb1c449c2a479e
Galanggg <p0ser@server17.stikom.edu>
2018-12-28 19:44:18 +0800

    Added item on dropdown !

--- js/script.js
+++ js/script.js
@@ -7,9 +7,20 @@ document.addEventListener('DOMContentLoaded', function () {
   let elemss = document.querySelectorAll('.dropdown-trigger');
   var instancesdrop1 = M.Dropdown.init(elemss, {
     coverTrigger: false,
-    alignment: 'left'
+    alignment: 'left',
+    constrainWidth: false
   });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  let elemscalendar = document.querySelectorAll('.datepicker');
+  let instances = M.Datepicker.init(elemscalendar, {
+    selectMonths: true,
+    selectYears: 100,
+    format: 'dd/mmm/yyyy',
+  });
+});
+
 
 document.addEventListener('DOMContentLoaded', function () {
   var carousell = document.querySelector('.carousel.carousel-slider');
